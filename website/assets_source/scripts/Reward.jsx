9416828df6b1022daefcd2af6b8dab11620fb8bd
riwang <riwang@umich.edu>
2024-08-29 18:12:30 -0400

    Updating Reward component for times goal met

--- website/assets_source/scripts/Reward.jsx
+++ website/assets_source/scripts/Reward.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 
-function Reward({ name, desc, goal, progress }) {
+function Reward({ name, desc, goal, progress, times_goal_met }) {
     const styles = {
         container: {
             width: "80%"
-        },
-        h3: {
-            fontFamily: "Consolas"
         }
     };
     
     return (
         <div style={ styles.container } className="reward">
             <h3 style={ styles.h3 }>{ name }</h3>
+            <h4>Times achieved: { times_goal_met }</h4>
             <p>{ desc }</p>
             <p>Current status: { progress }/{ goal }</p>
         </div>
--- website/assets_source/scripts/RewardList.jsx
+++ website/assets_source/scripts/RewardList.jsx
@@ -9,11 +9,12 @@ function RewardList({ rewards }) {
     for (const index in json_list) {
         const item = json_list[index];
         reward_list.push(<Reward 
-            key={ item.reward_name } 
+            key={ item.reward_id } 
             name={ item.reward_name }
             desc={ item.reward_desc }
             goal={ item.goal }
             progress={ item.progress }
+            times_goal_met={ item.times_goal_met }
         />);
     }
 
